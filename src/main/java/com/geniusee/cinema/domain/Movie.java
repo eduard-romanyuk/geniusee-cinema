e164f61e0bc4ff11148c28d0f0220f5be999d1bf
Eduard Romanyuk <eduard.romanyuk1990@gmail.com>
2020-12-19 08:14:38 +0200

    feat: add movie model

---
+++ src/main/java/com/geniusee/cinema/domain/Movie.java
@@ -0,0 +1,34 @@
+package com.geniusee.cinema.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "movie")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "duration_minutes")
+    private int durationMinutes;
+
+    public Movie(String name, String description, int durationMinutes) {
+        this.name = name;
+        this.description = description;
+        this.durationMinutes = durationMinutes;
+    }
+}
---
+++ src/main/java/com/geniusee/cinema/event/GenerateSampleMovies.java
@@ -0,0 +1,27 @@
+package com.geniusee.cinema.event;
+
+import com.geniusee.cinema.domain.Movie;
+import com.geniusee.cinema.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class GenerateSampleMovies implements ApplicationListener<ContextRefreshedEvent> {
+    private final MovieRepository movieRepository;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        List<Movie> movies = Arrays.asList(
+                new Movie("On the Rocks", null, 100),
+                new Movie("The Trip to Greece", null, 130),
+                new Movie("Lost Girls", null, 115)
+        );
+        movieRepository.saveAll(movies);
+    }
+}
