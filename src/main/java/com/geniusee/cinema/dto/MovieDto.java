3c11dd727401b31f710ce00acce98b744c10b09a
Eduard Romanyuk <eduard.romanyuk1990@gmail.com>
2020-12-20 17:04:45 +0200

    feat: add movie dto validation

--- src/main/java/com/geniusee/cinema/dto/MovieDto.java
+++ src/main/java/com/geniusee/cinema/dto/MovieDto.java
@@ -4,11 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class MovieDto {
+    @NotBlank(message = "Name is mandatory")
     private String name;
     private String description;
+    @Min(value = 0, message = "Duration cannot be negative")
     private int durationMinutes;
 }
