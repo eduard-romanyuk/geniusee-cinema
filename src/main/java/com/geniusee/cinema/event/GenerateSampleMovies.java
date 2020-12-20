a24afa9bb32b9833cc8b81fa920afb38a672c3e6
Eduard Romanyuk <eduard.romanyuk1990@gmail.com>
2020-12-20 17:07:14 +0200

    fix(context loading test): don't insert sample data for unit-test profile

--- src/main/java/com/geniusee/cinema/event/GenerateSampleMovies.java
+++ src/main/java/com/geniusee/cinema/event/GenerateSampleMovies.java
@@ -4,6 +4,7 @@ import com.geniusee.cinema.domain.Movie;
 import com.geniusee.cinema.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@Profile("!unit-test")
 @RequiredArgsConstructor
 public class GenerateSampleMovies implements ApplicationListener<ContextRefreshedEvent> {
     private final MovieRepository movieRepository;
